1887d60c13fe1c42cc2c3106d1b13f0c0456293f
felipe-botura <felipebotura7@gmail.com>
2024-10-10 12:25:13 -0300

    Criado o projeto, feito o HTML e CSS

---
+++ main.js
@@ -0,0 +1,5 @@
+const palavras = ["loopar", "palet", "gerador", "escotilha", "assassino", "sobrevivente"];
+let tentativasUsadas = 0;
+let letras = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+let palavraSecreta, palavraOculta;
+let jogando;
